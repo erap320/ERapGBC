e49654e8b7157afb139440e842734a6c4eeb808a
ERap320 <battistonelia@erap.space>
2020-11-19 00:07:20 +0100

    Implemented better debugger commands

--- ERapGBC/main.cpp
+++ ERapGBC/main.cpp
@@ -1,35 +1,53 @@
 #include "architecture.hpp"
 #include <iostream>
 using std::cout;
+using std::cin;
+using std::getline;
 using std::endl;
 
 int main()
 {
 	Architecture* arch = Architecture::instance();
 
-	arch->print_registers();
-	arch->print_instruction(0, disasm(0));
-	//std::cin.get();
-
-	while (arch->PC.to_ulong() != 0x9D)
-		arch->step(false);
-
-	char read;
+	string buffer;
 	while (true)
 	{
-		for (int i = 0; i < 50; i++) cout << endl;
+		for (int i = 0; i < 10; i++) cout << endl;
 		arch->step(true);
 
-		read = std::cin.get();
+		cout << "\n> ";
+		getline(cin, buffer);
 
-		switch (read)
+
+		switch (buffer[0])
 		{
-		case 'c':
-			for (int i = 0; i < 100; i++)
-				arch->step(false);
-		case 'o':
+		case 'b': {
+			//Breakpoint
+			buffer.erase(0, 2);
+			if (!buffer.empty())
+			{
+				unsigned long breakpoint = strtoul(buffer.c_str(), NULL, 16) & 0xffff;
+				while (arch->PC.to_ulong() + disasm(arch->PC.to_ulong()).length() != breakpoint)
+					arch->step(false);
+			}
+			break;
+		}
+		case 'n': {
+			//Make n steps
+			buffer.erase(0, 2);
+			if (!buffer.empty())
+			{
+				unsigned long steps = strtoul(buffer.c_str(), NULL, 0);
+				for (unsigned int i = 0; i < steps; i++)
+					arch->step(false);
+			}
+			break;
+		}
+		case 'o': {
+			//Step out
 			while (disasm(arch->PC.to_ulong()).cmd != RET)
 				arch->step(false);
+		}
 		}
 	}
 
